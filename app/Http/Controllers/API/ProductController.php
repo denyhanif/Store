d7883b9559630c1ddeba7b74ab768bf666b49b2c
denyhanif <36994300+denyhanif@users.noreply.github.com>
2020-03-29 22:56:25 +0700

    produk api

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -29,5 +29,38 @@ class ProductController extends Controller
             else
                 return ResponFormater::error(null,'Data Produk Tidak Di Temukan',404);
         }
+        //slug
+        
+        if($id){
+            //query untuk slug
+            $product= Product::with('galleries')->where($slug)
+                                                ->first();//ambil data paliing pertama
+            //jika produk ditemukan
+            if($product)
+                return ResponFormater::success($product,'Data Product Berhasil Di ambil');
+            else
+                return ResponFormater::error(null,'Data Produk Tidak Di Temukan',404);
+        }
+
+        $product = Product::with('galleries');
+
+        //ex cari berdasarkan product name 
+        if($name)
+            $product->where('type','like','%'.name.'%');
+
+        if($type)
+            $product->where('type','like','%'.type.'%');
+
+        if($price_from)
+            $product->where('type','like','%'.price_from.'%');
+
+        if($price_to)
+            $product->where('type','like','%'.price_to.'%');
+
+        return ResponFormater::success(
+            $product->paginate($limit),
+            'Data list berhasi di tambah'
+        );
+
     }
 }
--- app/Http/Controllers/API/ResponFormater.php
+++ app/Http/Controllers/API/ResponFormater.php
@@ -18,7 +18,7 @@ class ResponFormater{
         self::$response['meta']['message']= $message; //self yaitu fungsi yang di gunakan untuk menggantikan this (kara ini pake method static)
         self::$response['data']=$data;
 
-        return response()->json(self::$response, self::$response['meta']['code']);
+        return response()->json(self::$response, self::$response['meta']['code']);//fromat
     }
 
     public static function error($data=null, $message= null, $code = 400){
@@ -27,7 +27,7 @@ class ResponFormater{
         self::$response['meta']['message']=$message;
         self::$response['data']=$data;
         self::$response['meta']['code']=$code;
-        return response()->json(self::$response, self::$response['meta']['code']);
+        return response()->json(self::$response, self::$response['meta']['code']);//fromat
 
 
     }
